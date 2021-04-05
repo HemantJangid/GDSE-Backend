395edcdaa61313d265779f6f760a3e043314996e
HemantJangid <jhemant539@gmail.com>
2021-04-05 21:46:06 +0530

    database

--- gdse/settings.py
+++ gdse/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'gdse.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'dev',
+        'NAME': 'prod',
         'USER': 'postgres',
         'PASSWORD': 'jrBthEC!ZUi4',
         'HOST': 'gdse.clsrq9deqixe.ap-south-1.rds.amazonaws.com',
